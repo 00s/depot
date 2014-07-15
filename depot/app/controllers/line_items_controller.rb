5480cbf066b69d0432d84e1a9f48fb5533355ed4
Rebeca Souza <rebeca_souza85@hotmail.com>
2014-07-15 17:38:04 -0300

    adding a remove item button to cart

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -57,9 +57,17 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+  # Removes the product from the cart
+    if @line_item.quantity == 1
+      @line_item.destroy
+  # Removes one Item from the cart
+    else
+      @line_item.quantity -=1
+      @line_item.save
+    end
+
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
