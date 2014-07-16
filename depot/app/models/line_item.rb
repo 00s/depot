69981f1cbab1ed43f765e76153a30f9032ce2b63
Rebeca Souza <rebeca_souza85@hotmail.com>
2014-07-15 22:07:08 -0300

    alteracao no metodo destroy de product. alert bootstrap style cart styling. line_item new methods

--- depot/app/models/line_item.rb
+++ depot/app/models/line_item.rb
@@ -2,7 +2,16 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
 
+	def title
+		product.title
+	end
+
+	def price
+		product.price
+	end
+
   def total_price
 		product.price * quantity
 	end
+
 end
