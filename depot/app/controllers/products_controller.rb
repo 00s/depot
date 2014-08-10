5bf6076288329928ba201bb49b7770fd009aa7ba
Adolfo Farias <adf.melo@gmail.com>
2014-08-10 19:17:43 -0300

    hide cart if its empty

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   include CurrentCart
 
-  before_action :set_cart, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_cart
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
