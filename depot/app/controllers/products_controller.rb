693566d92f1cf7567f35ff0861f04b8080e0d8ba
Adolfo Farias <adf.melo@gmail.com>
2014-08-10 17:51:17 -0300

    add jquery animation when picking a book

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+  include CurrentCart
+
+  before_action :set_cart, only: [:index, :show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
