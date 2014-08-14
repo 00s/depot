325a36329ecc5a1b3f490e389aaa513b04c51322
Adolfo Farias <adf.melo@gmail.com>
2014-08-14 20:35:27 -0300

    iteration g2

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
   resources :carts
 
   get 'store/index'
-
-  resources :products
+  resources :products do
+    member do
+      get :who_bought
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
